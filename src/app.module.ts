7ad5d4c4f47b13a29debae76300db4f0c31957cf
jjzapata <jjzapata@bancolombia.com.co>
2024-05-06 17:19:18 -0500

    00-vuelos-init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { FlightsModule } from './flights/flights.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type:'postgres',
-      host: process.env.APP_DB_HOST,
-      port: +process.env.APP_DB_PORT,
-      database: process.env.APP_DB_DATABASE,
-      username: process.env.APP_DB_USERNAME,
-      password: process.env.APP_DB_PASSWORD,
-      autoLoadEntities:true,
-      synchronize:true
-    }),
-    FlightsModule,
-    AuthModule
-  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/flights/dto/create-flight.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateFlightDto {
-
-    from:string;
-    to:string
-}
